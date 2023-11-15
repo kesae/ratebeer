562f81832cefa4f6bc68f28b003e8bb7a4a3d820
kesae <keikkosa@outlook.com>
2023-11-15 23:09:17 +0200

    undo redirect to other host

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,6 +9,6 @@ class RatingsController < ApplicationController
 
   def create
     Rating.create params.require(:rating).permit(:score, :beer_id)
-    redirect_to "http://www.cs.helsinki.fi"
+    redirect_to ratings_path
   end
 end
