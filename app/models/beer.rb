3773c7fb663758d27682c1c95b5a7101a2d200da
kesae <keikkosa@outlook.com>
2023-11-15 19:03:15 +0200

    2.4, 2.5  add average rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        self.ratings.map { |r| r.score}.reduce(:+) / self.ratings.count.to_f
+    end
 end
