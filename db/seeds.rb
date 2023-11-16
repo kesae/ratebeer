0f261a71043208b81a2fa5234bfbc3902b88495c
kesae <keikkosa@outlook.com>
2023-11-16 23:47:33 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,23 @@ b1 = Brewery.create name: "Koff", year: 1897
 b2 = Brewery.create name: "Malmgard", year: 2001
 b3 = Brewery.create name: "Weihenstephaner", year: 1040
 
-b1.beers.create name: "Iso 3", style: "Lager"
-b1.beers.create name: "Karhu", style: "Lager"
-b1.beers.create name: "Tuplahumala", style: "Lager"
-b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
-b2.beers.create name: "X Porter", style: "Porter"
-b3.beers.create name: "Hefeweizen", style: "Weizen"
-b3.beers.create name: "Helles", style: "Lager"
+beer1 = b1.beers.create name: "Iso 3", style: "Lager"
+beer2 = b1.beers.create name: "Karhu", style: "Lager"
+beer3 = b1.beers.create name: "Tuplahumala", style: "Lager"
+beer4 = b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
+beer5 = b2.beers.create name: "X Porter", style: "Porter"
+beer6 = b3.beers.create name: "Hefeweizen", style: "Weizen"
+beer7 = b3.beers.create name: "Helles", style: "Lager"
+
+beer1.ratings.create score: 12
+beer1.ratings.create score: 40
+beer1.ratings.create score: 30
+beer2.ratings.create score: 46
+beer2.ratings.create score: 12
+beer3.ratings.create score: 22
+beer4.ratings.create score: 12
+beer4.ratings.create score: 40
+beer4.ratings.create score: 30
+beer6.ratings.create score: 46
+beer6.ratings.create score: 12
+beer7.ratings.create score: 22
