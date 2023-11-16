b94004a19c905c08af043067374a8020c3282155
kesae <keikkosa@outlook.com>
2023-11-16 21:41:37 +0200

    2.14 add brewery ratings

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,7 @@
 class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
+    def average_rating
+        self.ratings.map { |r| r.score}.reduce(:+) / self.ratings.count.to_f
+    end
 end
