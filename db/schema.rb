1d154dd79812848142efec2ab4ca6b9b93524c89
kesae <keikkosa@outlook.com>
2023-11-20 17:28:48 +0200

    scaffold and migrate Membership

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_20_151423) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_152440) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_20_151423) do
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "beer_club_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
