cf1d8816ec435fd3f13f0a771b8bcbaf4f245369
kesae <keikkosa@outlook.com>
2023-11-20 16:38:12 +0200

    3.8 improve brewery year validation

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,5 +3,12 @@ class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
   validates :name, presence: true
-  validates :year, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 1040, less_than_or_equal_to: 2022 }
+  validates :year, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 1040 }
+  validate :year_cannot_be_in_the_future
+
+  def year_cannot_be_in_the_future
+    return unless year.present? && year.to_i > Time.now.year
+
+    errors.add(:year, "can't be in the future")
+  end
 end
