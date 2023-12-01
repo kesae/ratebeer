00e1059562bdf1a6831cacd2526db53c280e6621
kesae <keikkosa@outlook.com>
2023-12-01 21:22:26 +0200

    4.1 add tests for passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,20 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
 
+  it "is not saved with a too short password" do
+    user = User.create username: "Pekka", password: "1Aa"
+
+    expect(user).not_to be_valid
+    expect(User.count).to eq(0)
+  end
+
+  it "is not saved with an all-lowercase password" do
+    user = User.create username: "Pekka", password: "longenoughpassword"
+
+    expect(user).not_to be_valid
+    expect(User.count).to eq(0)
+  end
+
   describe "with a proper password" do
     let(:user){ User.create username: "Pekka", password: "Secret1", password_confirmation: "Secret1" }
     let(:test_brewery) { Brewery.new name: "test", year: 2000 }
