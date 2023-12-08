0aebfeff652c58fa7669c469bd213856c8ca3e94
kesae <keikkosa@outlook.com>
2023-12-08 12:41:04 +0200

    move add_many method to helpers

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,4 +1,27 @@
 module Helpers
+  private
+  def arg_lists_to_hashes(arg_lists)
+    hashes = []
+    arg_lists.first[1].each_with_index do |_, i|
+      hash = {}
+      arg_lists.keys.each do |key|
+        hash = hash.merge({key => arg_lists[key][i]})
+      end
+      hashes << hash
+    end
+    hashes
+  end
+
+  def create_many(type, common_args, arg_lists)
+    result = []
+    hashes = arg_lists_to_hashes(arg_lists)
+    hashes.each do |hash|
+      obj = FactoryBot.create(type, common_args.merge(hash))
+      result << obj
+    end
+    result
+  end
+
 
   def sign_in(credentials)
     visit signin_path
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,6 @@
 require 'rails_helper'
 
-def arg_lists_to_hashes(arg_lists)
-  hashes = []
-  arg_lists.first[1].each_with_index do |_, i|
-    hash = {}
-    arg_lists.keys.each do |key|
-      hash = hash.merge({key => arg_lists[key][i]})
-    end
-    hashes << hash
-  end
-  hashes
-end
-
-def create_many(type, common_args, arg_lists)
-  result = []
-  hashes = arg_lists_to_hashes(arg_lists)
-  hashes.each do |hash|
-    obj = FactoryBot.create(type, common_args.merge(hash))
-    result << obj
-  end
-  result
-end
-
+include Helpers
 
 RSpec.describe User, type: :model do
   it "has the username set correctly" do
