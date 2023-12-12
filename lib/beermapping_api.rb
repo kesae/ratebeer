551a0b493f155404462dc20b1dc41de82eb56172
kesae <keikkosa@outlook.com>
2023-12-12 11:21:14 +0200

    fix style

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -5,7 +5,7 @@ class BeermappingApi
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places.is_a?(Hash) and places['id'].nil?
+    return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
     places.map do |place|
