bba8e777a428f57a3e6b091964a0d4741dc06450
kesae <keikkosa@outlook.com>
2023-12-12 10:41:24 +0200

    add webmock to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'helpers'
+require 'webmock/rspec'
 require 'simplecov'
 SimpleCov.start('rails')
 # Add additional requires below this line. Rails is not loaded until this point!
