4f648e3e33dec09bcf84ee133632d54965a5f79d
kesae <keikkosa@outlook.com>
2023-12-12 10:25:16 +0200

    5.2 add Beermapping API tests

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Places" do
-  it "if one is returned by the API, it is shown at the page" do
+  it "if one is returned by the API, it is shown on the page" do
     allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
       [ Place.new( name: "Oljenkorsi", id: 1 ) ]
     )
@@ -11,5 +11,28 @@ describe "Places" do
     click_button "Search"
 
     expect(page).to have_content "Oljenkorsi"
+  end
+  it "if returned by the API, are shown on the page" do
+    allow(BeermappingApi).to receive(:places_in).with("helsinki").and_return(
+      [
+        Place.new( name: "Oljenkorsi", id: 1 ),
+        Place.new( name: "Kronikka", id: 2 )
+      ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'helsinki')
+    click_button "Search"
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Kronikka"
+  end
+  it "if no locations returned by the API, tells it in a notice" do
+    place = "oulu"
+    allow(BeermappingApi).to receive(:places_in).with(place).and_return([])
+
+    visit places_path
+    fill_in('city', with: place)
+    click_button "Search"
+    expect(find("#notice")).to have_content "No locations in #{place}"
   end
 end
