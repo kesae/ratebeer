1613f8439919e851087bf8db2a35f593f826e0ae
kesae <keikkosa@outlook.com>
2023-12-15 16:59:50 +0200

    5.11-5.13 create style table

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,8 +3,8 @@ class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
   has_many :raters, through: :ratings, source: :user
+  belongs_to :style
   validates :name, presence: true
-  validates :style, presence: true
 
   def to_s
     "#{name}, #{brewery.name}"
