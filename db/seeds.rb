23bca064942bb00730b7e33bbc9bd37baca29f92
kesae <keikkosa@outlook.com>
2023-12-15 18:04:43 +0200

    5.11-5.13 create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-b1 = Brewery.create name: "Koff", year: 1897
-b2 = Brewery.create name: "Malmgard", year: 2001
-b3 = Brewery.create name: "Weihenstephaner", year: 1040
 
-beer1 = b1.beers.create name: "Iso 3", style: "Lager"
-beer2 = b1.beers.create name: "Karhu", style: "Lager"
-beer3 = b1.beers.create name: "Tuplahumala", style: "Lager"
-beer4 = b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
-beer5 = b2.beers.create name: "X Porter", style: "Porter"
-beer6 = b3.beers.create name: "Hefeweizen", style: "Weizen"
-beer7 = b3.beers.create name: "Helles", style: "Lager"
-
-beer1.ratings.create score: 12
-beer1.ratings.create score: 40
-beer1.ratings.create score: 30
-beer2.ratings.create score: 46
-beer2.ratings.create score: 12
-beer3.ratings.create score: 22
-beer4.ratings.create score: 12
-beer4.ratings.create score: 40
-beer4.ratings.create score: 30
-beer6.ratings.create score: 46
-beer6.ratings.create score: 12
-beer7.ratings.create score: 22
+if ActiveRecord::Base.connection.column_exists?(:beers, :old_style)
+  Beer.all.each do |beer|
+    style = Style.find_or_create_by!(name: beer.old_style, description: "No description yet.")
+    beer.style_id = style.id
+    beer.save
+  end
+end
