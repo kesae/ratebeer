89b58e390a11b481de7ca5ab629d73a895299a2f
kesae <keikkosa@outlook.com>
2023-12-20 22:38:06 +0200

    6.11-6.12 prevent closed account from signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,13 @@ class SessionsController < ApplicationController
     user = User.find_by username: params[:username]
     # tarkastetaan että käyttäjä olemassa, ja että salasana on oikea
     if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome back!"
+      if user.closed
+        message = "Your account is closed, please contact our admins"
+        redirect_to signin_path, notice: message
+      else
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "Welcome back!"
+      end
     else
       redirect_to signin_path, notice: "Username and/or password mismatch"
     end
