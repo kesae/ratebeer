cd20156ed85102e7845618e383604b20b3f858f0
kesae <keikkosa@outlook.com>
2023-12-20 18:58:06 +0200

    6.10 create admin column to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_18_172932) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_160311) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_18_172932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,15 @@ if ActiveRecord::Base.connection.column_exists?(:beers, :old_style)
 end
 
 if Brewery.all.map(&:active).all?(nil)
-   Brewery.all.first(Brewery.all.size - 1).each do |brewery|
+  Brewery.all.first(Brewery.all.size - 1).each do |brewery|
     if brewery.active.nil?
       brewery.active = true
       brewery.save
     end
   end
+end
+
+if User.all.map(&:admin).all?(nil)
+  user = User.find_by username: "admin"
+  user.update_attribute(:admin, true) if user
 end
