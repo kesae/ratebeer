f49510f13f57b6ff53f1dfd688f46fdf426d55f7
kesae <keikkosa@outlook.com>
2023-12-20 20:11:08 +0200

    6.10 show destroy buttons for admins only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,15 @@ module ApplicationHelper
     return if current_user.nil?
 
     edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
-    del = link_to('Destroy', item, method: :delete,
-                                   form: { data: { turbo_confirm: "Are you sure ?" } },
-                                   class: "btn btn-danger")
-    raw("#{edit} #{del}")
+    if current_user.admin
+      del = link_to('Destroy', item, {
+                      method: :delete,
+                      form: { data: { turbo_confirm: "Are you sure ?" } },
+                      class: "btn btn-danger"
+                    })
+      return raw("#{edit} #{del}")
+    end
+    raw(edit.to_s)
   end
 
   def round(number)
