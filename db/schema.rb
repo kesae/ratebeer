0fd376111c648aa8372d8569f1d9b63e2647ecf2
kesae <keikkosa@outlook.com>
2023-12-23 19:13:47 +0200

    add index to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_185321) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_23_170407) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_185321) do
     t.string "password_digest"
     t.boolean "admin"
     t.boolean "closed"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
